5675ec81169678d8d4c5fb694c0898ac4b6764ad
Petros Ohanyan <petrosmu24@gmail.com>
2020-08-31 00:27:22 +0400

    test

--- src/Blog_Test_Petros_Ohanyan.Application/AppConsts.cs
+++ src/Blog_Test_Petros_Ohanyan.Application/AppConsts.cs
@@ -6,5 +6,6 @@
         /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
         /// </summary>
         public const string DefaultPassPhrase = "gsKxGZ012HLL3MI5";
+        public int MyProperty { get; set; }
     }
 }
